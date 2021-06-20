7e882d9b9d4fce7079eb33cd80551e9839a3eafb
jkL-snk <79194364+jkL-snk@users.noreply.github.com>
2021-06-20 14:43:51 +0700

    Update import.sh

--- jenkins/jobs/import.sh
+++ jenkins/jobs/import.sh
@@ -2,6 +2,6 @@
 for f in *.xml;
 do
 echo "Processing ${f%.*} file.."; #truncate the .xml extention and load the xml file for job creation
-java -jar jenkins-cli.jar -s http://admin:f6e891122e6c4002ae129d6b1d2fe834@178.154.230.160:8080/ create-job ${f%.*}  < $f
+java -jar jenkins-cli.jar -s http://admin:<auth-string>@<hostname>:8080/ create-job ${f%.*}  < $f
 done
 
