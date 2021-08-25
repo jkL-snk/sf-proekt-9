1fa02084621b838a11137c3ed783d6259a6ed441
Evgeny Malyshev <meo@bakaneko.ru>
2021-08-25 14:44:20 +0000

    cleanup

--- jenkins/jobs/import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for f in *.xml;
-do
-echo "Processing ${f%.*} file.."; #truncate the .xml extention and load the xml file for job creation
-java -jar jenkins-cli.jar -s http://admin:<auth-string>@<hostname>:8080/ create-job ${f%.*}  < $f
-done
-
